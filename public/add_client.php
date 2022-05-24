393b8564f5a9230a17fe8d017ce9f22d0c628c13
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-24 17:35:13 -0300

    Removed labels and set placeholders.

--- public/add_client.php
+++ public/add_client.php
@@ -9,21 +9,18 @@
 		<?= require 'navbar.html'; ?>
 
 		<div class="container">
-			<h1>Cadastro de cliente</h1>
 			<div class="form-container">
+				<h1>Cadastro de cliente</h1>
 
 				<form action="add.php" method="POST">
 					<div class="form-group">
-						<label for="client_name">Nome:</label>
-						<input name="client_name" id="client_name" type="text">
+						<input name="client_name" id="client_name" type="text" placeholder="Nome">
 					</div>
 					<div class="form-group">
-						<label for="client_name">Nome:</label>
-						<input name="client_name" id="client_name" type="text">
+						<input name="client_name" id="client_name" type="text" placeholder="E-mail">
 					</div>
 					<div class="form-group">
-						<label for="client_name">Nome:</label>
-						<input name="client_name" id="client_name" type="text">
+						<input name="client_name" id="client_name" type="text" placeholder="Telefone">
 					</div>
 					<input type="submit" value="Cadastrar">
 				</form>
