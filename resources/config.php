2398927bcd6a4fa8461e519e1a52bf14aab276a0
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-24 18:28:15 -0300

    Project configuration file.

---
+++ resources/config.php
@@ -0,0 +1,15 @@
+<?php
+/* Configuration file. This file contains automated settings to be used on the
+ * whole project when need, such as database credentials. */
+
+$config = array(
+	"db" => array(
+		"dbname" => "client-database",
+		"username" => "foobar",
+		"password" => "foobar",
+		"host" => "localhost"
+	)
+);
+
+// EOF
+
