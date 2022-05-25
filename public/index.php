ecd1c004f2fd6b80542a163bbdb909a24ff8fb10
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-25 14:27:40 -0300

    Main page, which fills the table with registered users.

--- public/index.php
+++ public/index.php
@@ -20,9 +20,18 @@
 					<th>Opções</th>
 				</tr>
 
-				<?= require_once '../resources/query.php'; ?>
-				
-
+				<!-- Fill table with data -->
+				<?php
+					require_once '../resources/query.php';
+					for ($i = 0; $i < count($user_list); $i++) {
+						echo '<tr>';
+						for ($j = 0; $j < count($user_list[$i]); $j++) {
+							echo '<td>' . $user_list[$i][$j] . '</td>';
+						}
+						echo '<td><button>Editar</button><button>Remover</button></td>';
+						echo '</tr>';
+					}
+				?>
 			</table>
 		</main>
 
