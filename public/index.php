9d0baf6a6858ca4600746ca69b1fd1205a84bd5c
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-26 18:05:11 -0300

    Client list to be continued.

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Gerenciamento de cliente</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body id="client-list-page">
+		<!-- Reusable header -->
+		<?php require_once 'header.php'; ?>
+
+		<!-- Main content area -->
+		<div class="main-content">
+			<!-- TODO -->				
+		</div>
+	</body>
+</html>
