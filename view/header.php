a4a66e5087422b83c171fc3f8047578c1c2ab772
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-06-06 11:25:25 -0300

    Header contains style specification, and another navbar link.

--- view/header.php
+++ view/header.php
@@ -1,6 +1,10 @@
 <!-- This is the reusable header for the system. -->
 
 <!DOCTYPE html>
+<head>
+	<link rel="stylesheet" href="/view/css/style.css">
+</head>
+
 <div id="reusable-header">
 	<div class="logo-area">
 		<h1><a href="#" class="link-light">Gerenciamento de clientes</a></h1>
@@ -8,7 +12,8 @@
 	
 	<div class="options">
 		<ul>
-			<li><a href="#" class="link-light">Adicionar</a>
+			<li><a href="/view/addClient.php" class="link-light">Adicionar</a>
+			<li><a href="index.php" class="link-light">Lista de clientes</a>
 		</ul>
 	</div>
 </div>
--- view/index.php
+++ view/index.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Gerenciamento de cliente</title>
-		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="view/css/style.css">
 	</head>
 	<body id="client-list-page">
 		<!-- Reusable header -->
